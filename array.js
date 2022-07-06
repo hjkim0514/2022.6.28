a905e47445bffd99edd40eae7b2d311253870a0c
hjkim0514 <97289374+hjkim0514@users.noreply.github.com>
2022-07-06 15:35:15 +0900

    Add files via upload

---
+++ array.js
@@ -0,0 +1,13 @@
+let groceries = ['bread', 'milk', 'eggs', 'ice cream', 'vegetables', 'cheese']; 
+console.log(groceries[3])
+groceries.push('cookies')
+console.log(groceries)
+
+let studentRoster = [
+    ["Cheetah", 12, 88],
+    ["Evee", 11, 97.5],
+    ["Rei", 13, 65.5]
+]
+console.log(studentRoster);
+console.log(studentRoster[0][1]);
+console.log(studentRoster[0][1]);
---
+++ functions.js
@@ -0,0 +1,10 @@
+function evenNumber(x){
+    if(x % 2 ==0){
+        console.log(true)
+    }
+    else{
+        console.log(false)
+    }
+}
+
+evenNumber(11)
